81bc6fd6389fa145e48130aabc5b807ebaf51fa0
Your Name <your.email@example.com>
2025-02-11 08:30:28 +0700

    fix: Update CORS configuration and API URLs

--- FE/src/config/api.js
+++ FE/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://shoes-fe-v3-fprf-7i989ua76-hata214s-projects.vercel.app/api/v1';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://shoes-fe-v3-backend-bdh0hh1t1-hata214s-projects.vercel.app/api/v1';
 
 export const endpoints = {
     products: '/products',
